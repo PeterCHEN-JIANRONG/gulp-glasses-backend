6b181c88497c6d09566dc1030156fb13f39166fd
peter.chen1024 <u0052017@gmail.com>
2021-08-03 17:50:37 +0800

    Update 2021-08-03T09:50:34.749Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,25 @@
 "use strict";
 
-console.log('Hello!');
+console.log('Hello!'); // DOM
+
+var navbarList = document.querySelector(".navbar-nav");
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $(".pagination").click(function (e) {
+    e.preventDefault(); // $(".page-item").toggleClass("active");
+
+    $(".page-item").removeClass('active');
+    e.target.parentNode.classList.add('active');
+    console.log(e.target.nodeName);
+  }); // navbarList.addEventListener('click', (e) => {
+  // e.preventDefault();
+  // let link = null;
+  // if (e.target.nodeName === 'A') {
+  //   link = e.target;
+  // }
+  // else if (e.target.nodeName === 'I') {
+  //   link = e.target.parentNode;
+  // }
+  // link.classList.add('active');
 });
 //# sourceMappingURL=all.js.map

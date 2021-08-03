72485337bc4da8ee8f51df54097cc072b94d7603
peter.chen1024 <u0052017@gmail.com>
2021-08-03 17:50:16 +0800

    add admin頁,樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,31 @@
 console.log('Hello!');
+// DOM
+const navbarList = document.querySelector(".navbar-nav");
+
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
-});
+
+  $(".pagination").click(function (e) {
+    e.preventDefault();
+    // $(".page-item").toggleClass("active");
+    $(".page-item").removeClass('active');
+    e.target.parentNode.classList.add('active')
+    console.log(e.target.nodeName);
+
+  });
+
+
+  // navbarList.addEventListener('click', (e) => {
+  // e.preventDefault();
+
+  // let link = null;
+  // if (e.target.nodeName === 'A') {
+  //   link = e.target;
+  // }
+  // else if (e.target.nodeName === 'I') {
+  //   link = e.target.parentNode;
+  // }
+  // link.classList.add('active');
+
+})

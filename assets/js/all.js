69098dfafa9a421dffd8485166b9b625e451241b
peter.chen1024 <u0052017@gmail.com>
2021-08-05 16:49:32 +0800

    Update 2021-08-05T08:49:30.675Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -11,15 +11,40 @@ $(document).ready(function () {
     $(".page-item").removeClass('active');
     e.target.parentNode.classList.add('active');
     console.log(e.target.nodeName);
-  }); // navbarList.addEventListener('click', (e) => {
-  // e.preventDefault();
-  // let link = null;
-  // if (e.target.nodeName === 'A') {
-  //   link = e.target;
-  // }
-  // else if (e.target.nodeName === 'I') {
-  //   link = e.target.parentNode;
-  // }
-  // link.classList.add('active');
+  });
+  $(".reply_btn").click(function (e) {
+    e.preventDefault(); // // $(".page-item").toggleClass("active");
+    // $(".page-item").removeClass('active');
+    // e.target.parentNode.classList.add('active')
+
+    $(".own_message").toggleClass("d-none");
+    $("#control_menu").toggleClass("d-none");
+    $(".card-edit").toggleClass("d-none");
+    $("#reply_text").focus();
+  });
+  $(".cancel_btn").click(function (e) {
+    e.preventDefault();
+    $(".own_message").toggleClass("d-none");
+    $("#control_menu").toggleClass("d-none");
+    $(".card-edit").toggleClass("d-none");
+  });
+  $(".expand-icon").click(function (e) {
+    e.preventDefault();
+    $(".expand-btn").toggleClass("d-none");
+  });
+}); // from表單驗證提示訊息
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+
+var forms = document.querySelectorAll('.needs-validation'); // Loop over them and prevent submission
+
+Array.prototype.slice.call(forms).forEach(function (form) {
+  form.addEventListener('submit', function (event) {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+  }, false);
 });
 //# sourceMappingURL=all.js.map

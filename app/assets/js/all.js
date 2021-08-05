0fc8ad74a43585a305956714b1402c007d1c6c17
peter.chen1024 <u0052017@gmail.com>
2021-08-05 16:52:04 +0800

    delete 無用程式碼

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,5 @@
 console.log('Hello!');
 // DOM
-const navbarList = document.querySelector(".navbar-nav");
-
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
@@ -11,15 +9,11 @@ $(document).ready(() => {
     // $(".page-item").toggleClass("active");
     $(".page-item").removeClass('active');
     e.target.parentNode.classList.add('active')
-    console.log(e.target.nodeName);
   });
 
 
   $(".reply_btn").click(function (e) {
     e.preventDefault();
-    // // $(".page-item").toggleClass("active");
-    // $(".page-item").removeClass('active');
-    // e.target.parentNode.classList.add('active')
     $(".own_message").toggleClass("d-none");
     $("#control_menu").toggleClass("d-none");
     $(".card-edit").toggleClass("d-none");
